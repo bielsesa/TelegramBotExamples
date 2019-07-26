591fb906f4807890213805eb5445f87234b0f923
Biel Serrano Sanchez <bielsesa@gmail.com>
2019-07-26 08:16:57 +0200

    Added video messages.

--- Program.cs
+++ Program.cs
@@ -30,6 +30,7 @@ namespace TelegramBot
         {
             // Normal text message. Checks wether the message does have text.
             // in case it does, the bot answers.
+            #region Text Message
             if (e.Message.Text != null)
             {
                 Console.WriteLine($"Received text message in chat {e.Message.Chat.Id}.");
@@ -40,9 +41,11 @@ namespace TelegramBot
                         text: "You said:\n" + e.Message.Text
                     );
             }
+            #endregion
 
             // Normal text message, with all the properties 
             // a text message can have.
+            #region Text message with properties
             Message textMessage = await botClient.SendTextMessageAsync(
               chatId: e.Message.Chat, // or a chat id: 123456789
               text: "Trying *all the parameters* of `sendMessage` method",
@@ -54,29 +57,35 @@ namespace TelegramBot
                 "https://core.telegram.org/bots/api#sendmessage"
               ))
             );
+            #endregion
 
             // Almost all of the methods for sending messages return you 
             // the message you just sent.
+            #region Messages return you the message you sent (as an object)
             Console.WriteLine(
               $"{textMessage.From.FirstName} sent message {textMessage.MessageId} " +
               $"to chat {textMessage.Chat.Id} at {textMessage.Date.ToLocalTime()}. " + // if you don't use ToLocalTime(), the bot will show the UTC time.
               $"It is a reply to message {textMessage.ReplyToMessage.MessageId} " +
               $"and has {textMessage.Entities.Length} message entities."
             );
+            #endregion
 
             // Photo message. It gets the image from a URL on the internet.
             // All images can have captions. You can use HTML or Markdown on them.
+            #region Photo Message
             Message photoMessage = await botClient.SendPhotoAsync(
               chatId: e.Message.Chat,
               photo: "https://github.com/TelegramBots/book/raw/master/src/docs/photo-ara.jpg",
               caption: "<b>Ara bird</b>. <i>Source</i>: <a href=\"https://pixabay.com\">Pixabay</a>",
               parseMode: ParseMode.Html
             );
+            #endregion
 
             // Sticker message. Sticker files should be in WebP format.
             // Sends two messages. The first one gets the sticker by passing HTTP URL
             // to WebP sticker file, and the second by reusing the file_id of the
             // first sticker (kinda like a cache?)
+            #region Sticker Messages
             Message stickerMessage1 = await botClient.SendStickerAsync(
               chatId: e.Message.Chat,
               sticker: "https://github.com/TelegramBots/book/raw/master/src/docs/sticker-fred.webp"
@@ -86,11 +95,13 @@ namespace TelegramBot
               chatId: e.Message.Chat,
               sticker: stickerMessage1.Sticker.FileId
             );
+            #endregion
 
             // Audio message. Opens it in Media Player. MP3 Format.
             // Telegram can read metadata from the MP3 file (that's why
             // there are commented properties; the bot gets the same
             // properties, but from the file metadata!)
+            #region Audio Message
             Message audioMessage = await botClient.SendAudioAsync(
                 e.Message.Chat,
                 "https://github.com/TelegramBots/book/raw/master/src/docs/audio-guitar.mp3"
@@ -100,10 +111,12 @@ namespace TelegramBot
                 duration: 91 // in seconds
                 */
             );
+            #endregion
 
             // Voice message. Not opened in Media Player (played directly
             // on the chat). OGG Format.
             // In this case, the OGG file is on our disk!
+            #region Voice Message
             Message voiceMessage;
             using (var stream = System.IO.File.OpenRead("C:/Users/Biel/source/repos/TelegramBot/voice-nfl_commentary.ogg"))
             {
@@ -112,6 +125,39 @@ namespace TelegramBot
                   voice: stream,
                   duration: 36
                 );
+            }
+            #endregion
+
+            // You can send MP4 files as a regular video or as a video note.
+            // Other video formats must be sent as a file.
+
+            // Video message. They can have caption, reply, and reply markup
+            // (like other multimedia messages)
+            // You can optionally specify the duration and the resolution
+            // of the video. In this case the video is streamed,
+            // meaning the user can partly watch the video on stream, without
+            // having to download it completely.
+            Message videoMessage = await botClient.SendVideoAsync(
+                    chatId: e.Message.Chat,
+                    video: "https://raw.githubusercontent.com/TelegramBots/book/master/src/docs/video-countdown.mp4",
+                    thumb: "https://raw.githubusercontent.com/TelegramBots/book/master/src/2/docs/thumb-clock.jpg",
+                    supportsStreaming: true
+             );
+
+            // Video note message. They are shown in circles to the user.
+            // They are usually short (1 minute or less).
+            // You can send a video note only by the video file or
+            // reusing the file_id of another video note.
+            // (sending it by its HTTP URL is not supported currently)
+            Message videoNoteMessage;
+            using (var stream = System.IO.File.OpenRead("C:/Users/step/Source/Repos/TelegramBotExamples/video-waves.mp4"))
+            {
+                videoNoteMessage = await botClient.SendVideoNoteAsync(
+                        chatId: e.Message.Chat,
+                        videoNote: stream,
+                        duration: 47,
+                        length: 360 // value of width/height
+                    );
             }
         }
     }
