f279ad91b703ddf8e516d24323a9e4a146a23ac3
Biel Serrano Sanchez <bielsesa@gmail.com>
2019-07-29 00:32:57 +0200

    Changed ListAndScheduleBot

--- ListAndScheduleBot.cs
+++ ListAndScheduleBot.cs
@@ -5,6 +5,7 @@ using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Exceptions;
 using System.Collections.Generic;
+using System.Timers;
 
 namespace TelegramBot
 {
@@ -15,6 +16,10 @@ namespace TelegramBot
 
         static void Main(string[] args)
         {
+            // gets the registered users from the DB
+            registeredUsers = SQLiteHelper.GetRegisteredUsers();
+
+            // gets the bot with its authorization token
             botClient = new TelegramBotClient("953705394:AAFtLctlHehkU9qdI06-QLYPr-cGHel0tQY");
             var me = botClient.GetMeAsync().Result;
 
@@ -29,13 +34,34 @@ namespace TelegramBot
             }
             catch (ApiRequestException)
             {
-                Console.WriteLine("The Bot token is invalid.");
+                Console.WriteLine("The Bot token is invalid. Press any key to close.");
                 Console.ReadLine();
                 return;
             }
 
+            // create timer to send the current time every 5 minutes
+            Timer timer = new Timer();
+            timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            //halfHourTimer.Interval = 1800000;
+            timer.Interval = 300000;
+            timer.Enabled = true;
+
             Console.WriteLine($"Bot number {me.Id} with name {me.FirstName} started. Press any key to close.");
             Console.ReadLine();
+        }
+
+        private async static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            if (registeredUsers.Count != 0)
+            {
+                foreach (KeyValuePair<long, string> pair in registeredUsers)
+                {
+                    await botClient.SendTextMessageAsync(
+                        chatId: pair.Key,
+                        text: String.Format("Hola! El tiempo actual es: {0:T}", DateTime.Now)
+                    );
+                }
+            }
         }
 
         // Keep alive method. In case the bot indicates a receive error, it starts it again.
@@ -53,28 +79,33 @@ namespace TelegramBot
         {
             long currentChatId = e.Message.Chat.Id;
 
-            // Check wether the user has already been registered or not.
+            // Check whether the user has already been registered or not.
             // If not, save the UserId (actually, it's the chat id)
             if (!registeredUsers.ContainsKey(currentChatId))
             {
-                registeredUsers.Add(currentChatId, "");
+                registeredUsers.Add(currentChatId, DateTime.Now.ToString());
+                SQLiteHelper.RegisterUser(currentChatId, DateTime.Now.ToString());
 
                 Console.WriteLine($"User registered with ID {currentChatId}.");
 
                 await botClient.SendTextMessageAsync
                 (
                     chatId: currentChatId,
-                    text: "You were registered!"
+                    //text: "You were registered!"
+                    text: "¡Te has registrado!"
                 );
             }
 
-            if (e.Message.Text == "/showusers")
+            if (e.Message.Text == "/createlist")
             {
-                string currentRegistUsers = "These are the currently registered users:";
+                string currentRegistUsers = "Great! Let's create a new priorities list, then.";
 
-                foreach (KeyValuePair<long, string> pair in registeredUsers)
+                if (registeredUsers.Count != 0)
                 {
-                    currentRegistUsers += $"\n- *{pair.Key}*";
+                    foreach (KeyValuePair<long, string> pair in registeredUsers)
+                    {
+                        currentRegistUsers += $"\n- {pair.Key}";
+                    }
                 }
 
                 await botClient.SendTextMessageAsync
